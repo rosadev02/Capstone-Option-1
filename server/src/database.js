00d342248f3714d54a2f9a7f1b41ae2207c8e0c7
Pedro Paulo Rosa <pedro.paulo.gomes.rosa@gmail.com>
2024-02-21 17:46:00 -0300

    Pushing

--- server/src/database.js
+++ server/src/database.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose");
-const User = require("./models/user.model");
 
-const connection = "mongodb://localhost:27017/banking-app";
-
-const connectDb = () => {
-  return mongoose.connect(connection);
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Outras opções do mongoose, se necessário
+    });
+    console.log("Conectado ao MongoDB!");
+  } catch (error) {
+    console.error("Erro ao conectar ao MongoDB:", error);
+    // Pode adicionar lógica para lidar com o erro, como tentar reconectar
+    // ou encerrar o aplicativo
+    process.exit(1);
+  }
 };
 
 module.exports = connectDb;
